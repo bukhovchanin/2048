8f789d39406efa571e54a2a7af5eb9711fca03eb
Alexey <radkevich.alexey@gmail.com>
2014-03-25 20:00:38 +0300

    Bot added

--- js/application.js
+++ js/application.js
@@ -1,4 +1,8 @@
+//var GM;
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  var GM = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+//  var bot = new Bot(GM);
+//  setInterval(function() {bot.makeTurn();}, 25);
+    Autoplay(GM);
 });
